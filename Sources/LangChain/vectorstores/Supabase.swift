c0b9c6f1d1fa2f68203f641820fa1af1711a8f3e
Oscar Deng <oscardeng@Oscars-Air.lan>
2025-08-28 19:03:55 -0700

    updated supabase syntax from old to new 2.5.1

--- Sources/LangChain/vectorstores/Supabase.swift
+++ Sources/LangChain/vectorstores/Supabase.swift
@@ -29,7 +29,7 @@ public class Supabase: VectorStore {
     
     public override func similaritySearch(query: String, k: Int) async -> [MatchedModel] {
         let params = SearchVectorParams(query_embedding: await embeddings.embedQuery(text: query), match_count: k)
-        let rpcQuery = client.database.rpc(fn: "match_documents", params: params)
+        let rpcQuery = client.rpc("match_documents", params: params)
 
         do {
             let response: [MatchedModel] = try await rpcQuery.execute().value // Where DataModel is the model of the data returned by the function
@@ -45,10 +45,10 @@ public class Supabase: VectorStore {
     public override func addText(text: String, metadata: [String: String]) async {
         let embedding = await embeddings.embedQuery(text: text)
         let insertData = DocModel(content: text, embedding: embedding, metadata: metadata)
-        let query = client.database
+        let query = client
             .from("documents")
-            .insert(values: insertData,
-                    returning: .representation) // you will need to add this to return the added data
+            .insert(insertData)
+            .select() // Return the inserted data
 //            .select(columns: "id") // specifiy which column names to be returned. Leave it empty for all columns
             .single() // specify you want to return a single value.
         
